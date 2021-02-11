5c1ea26860003bd382f7075e6367f2f9bbc42a06
DeorukhkarN <neel.deorukhkar@kantar.com>
2021-02-11 12:11:59 -0600

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Classifying whether customers are likely to be satisfied based on their demographics and inflight experience',
+    author='Neel Deorukhkar',
+    license='',
+)
